4fd904e63f13ae543fccae516010de69c0d722fc
edisonosorioj <edisonosorioj@gmail.com>
2016-07-26 22:58:33 -0500

    Login

--- php/conexion.php
+++ php/conexion.php
@@ -1,1 +1,17 @@
-conexion.php
+<?php
+
+$dbhostname = 'localhost';
+$dbusername = 'root';
+$dbpassword = '';
+$DB = "sistema_contable";
+
+$conn = new mysqli($dbhostname,$dbusername, $dbpassword,$DB); 
+
+if (mysqli_connect_errno()) {
+	exit('Fallo Conexion: ' . mysqli_connect_error());
+}
+ 
+return $conn;
+
+
+?>
