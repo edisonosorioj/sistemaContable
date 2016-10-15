73b27a77707f8f434d3b07853f2d394cfcf2f36f
Edison Osorio <edisonosorio@nusoft.com.co>
2016-10-15 18:44:55 -0500

    Imagenes en Imagen

--- version1.2/php/menu.php
+++ version1.2/php/menu.php
@@ -1,12 +1,12 @@
 <picture></picture>
 <nav>
 	<ul>
-		<li class='menu inicio'><a href="../inicio/inicio.php"><img src="../../img/inicio.png">INICIO</a></li>
-		<li class='menu'><a href="../megaCuenta/estadoCompras.php">CUENTAS</a></li>
-		<li class='menu'><a href="../inventario/index.php">INVENTARIO</a></li>
-		<li class='menu'><a href="../egresos/compras.php">EGRESOS</a></li>
-		<li class='menu'><a href="../ingresos/ingresos.php">INGRESOS</a></li>
-		<li class='menu'><a href="../cliente/clientes.php">CLIENTES</a></li>
+		<li class='menu'><a href="../inicio/inicio.php"><img src="../../img/inicio.png">INICIO</a></li>
+		<li class='menu'><a href="../cliente/clientes.php"><img src="../../img/reporte.png">CLIENTES</a></li>
+		<li class='menu'><a href="../egresos/compras.php"><img src="../../img/reporte.png">EGRESOS</a></li>
+		<li class='menu'><a href="../ingresos/ingresos.php"><img src="../../img/reporte.png">INGRESOS</a></li>
+		<li class='menu'><a href="../megaCuenta/estadoCompras.php"><img src="../../img/reporte.png">CUENTAS</a></li>
+		<li class='menu'><a href="../inventario/index.php"><img src="../../img/reporte.png">INVENTARIO</a></li>
 		<li id="salir">
 			<a href='logout.php' class='salir'>Salir</a>
 		</li>
