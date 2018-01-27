53d84beabb71da33080a9e09ab7812c8167eba3a
edisonosorioj <edisonosorioj@gmail.com>
2018-01-27 13:30:22 -0500

    detalles

--- version2.0/php/cliente/cliente.php
+++ version2.0/php/cliente/cliente.php
@@ -100,53 +100,6 @@ $html="<!DOCTYPE html>
 </head>
 <body class='dashboard-page'>
 
-	
-	<section class='wrapper scrollable'>
-		<nav class='user-menu'>
-			<a href='javascript:;' class='main-menu-access'>
-			<i class='icon-proton-logo'></i>
-			<i class='icon-reorder'></i>
-			</a>
-		</nav>
-		<section class='title-bar'>
-			<div class='logo'>
-				<h1><a href='index.html'><img src='images/logo.png' alt='' />LOGO</a></h1>
-			</div>
-			<div class='full-screen'>
-				<section class='full-top'>
-					<button id='toggle'><i class='fa fa-arrows-alt' aria-hidden='true'></i></button>	
-				</section>
-			</div>
-			<div class='w3l_search'>
-				<form action='#' method='post'>
-					<input id='search' type='text' name='search' value=''>
-					<button class='btn btn-default' type='submit' disabled='true'><i class='fa fa-search'></i></button>
-				</form>
-			</div>
-			<div class='header-right'>
-				<div class='profile_details_left'>	
-					<div class='profile_details'>		
-						<ul>
-							<li class='dropdown profile_details_drop'>
-								<a href='#' class='dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
-									<div class='profile_img'>	
-										<span class='prfil-img'><i class='fa fa-user' aria-hidden='true'></i></span> 
-										<div class='clearfix'></div>	
-									</div>	
-								</a>
-								<ul class='dropdown-menu drp-mnu'>
-									<li> <a href='#'><i class='fa fa-cog'></i> Configuración</a> </li> 
-									<li> <a href='#'><i class='fa fa-user'></i> Perfil</a> </li> 
-									<li> <a href='#'><i class='fa fa-sign-out'></i> Salir</a> </li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-					<div class='clearfix'> </div>
-				</div>
-			</div>
-			<div class='clearfix'> </div>
-		</section>
 		<div class='main-grid'>
 			<div class='agile-grids'>	
 				<!-- tables -->
