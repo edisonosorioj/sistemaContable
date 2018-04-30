04e4c86a36ba0e47524daeb0612a92f821075735
Edison Osorio <edisonosorio@nusoft.com.co>
2018-04-30 16:47:57 -0500

    Usuarios

--- version2.2/php/configuracion/actUsuario.php
+++ version2.2/php/configuracion/actUsuario.php
@@ -1,1 +1,41 @@
-actUsuario.php
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login'])) {
+
+	header("Location: ../inicio/session.php");
+	exit();
+	
+}
+
+require_once "../conexion.php";
+
+$conex = new conection();
+$result = $conex->conex();
+
+
+	$documento	=	$_POST['documento'];
+	$nombre		=	$_POST['nombre'];
+	$apellido	=	$_POST['apellido'];
+	$idrol	 	=	$_POST['rol'];
+	$login	 	=	$_POST['login'];
+	$password 	=	md5($_POST['password']);
+
+// Agrega nuevos usuarios según el formulario recibido
+	$query = mysqli_query($result,"UPDATE administradores SET documento = '$documento', nombre = '$nombre', apellido = '$apellido', idrol = '$idrol', login = '$login', contrasena = '$password';");
+
+//Según la respuesta de la inserción se da una respuesta en un alert 
+	if($query > 0){
+		$msg = "El usuario " . $nombres . " fue actualizado";
+	}else{
+		$msg = 'Error al editar el usuario. Intente nuevamente';
+	}
+		
+	$html = "<script>
+		window.alert('$msg');
+		opener.location.reload();
+		window.close();
+	</script>";	
+	
+echo $html;	
