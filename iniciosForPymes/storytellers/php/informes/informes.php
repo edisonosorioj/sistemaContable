e21a1c951648f5470548629a6002369ab44ca956
edisonosorioj <edisonosorioj@gmail.com>
2018-09-01 19:03:14 -0500

    Avances buenos en Wink y versión 2.2.

--- iniciosForPymes/storytellers/php/informes/informes.php
+++ iniciosForPymes/storytellers/php/informes/informes.php
@@ -17,68 +17,15 @@ require_once "../conexion.php";
 
 $conex = new conection();
 $result = $conex->conex();
-
-$show = '';
-
-if ($idrol == 0) {
-	include "../menu.php";
-}else{
-	include "../menu2.php";
-}
-
-// Consulta y por medio de un while muestra la lista de los pedidos
-
-$query2 = mysqli_query($result,'select p.cliente_id, p.pedido_id as pedido_id, c.nombres as nombres, p.nombre_pedido, p.t_costo, p.start, p.end, p.estado from pedidos p inner join clientes c on p.cliente_id = c.id where p.start BETWEEN CURDATE() AND NOW() and estado = 1');
-
+$tr = '';
 $tr2 = '';
 
- while ($row2 = $query2->fetch_array(MYSQLI_BOTH)){
-
- 	$estado = ($row2['estado'] == '0')?"Pendiente":"Realizado";
-
- 	$tr2 .=	"<tr class='rows' id='rows'>
-				<td>" . $row2['nombres'] 					. "</td>
-				<td>" . $row2['nombre_pedido'] 				. "</td>
-				<td  align='right'>$ " . $row2['t_costo'] 	. "</td>
-				<td>" . $row2['start']						. "</td>
-				<td>" . $row2['end']						. "</td>
-				<td>" . $estado								. "</td>
-				<td>&nbsp;&nbsp;
-				<a href='pedidoProductos.php?id=" . $row2['pedido_id'] . "'><span data-tooltip='Ver Detalles'>
-					<i class='fa fa-file-text-o'></i></spam></a>&nbsp;&nbsp;
-				</td>
-			</tr>";
-
- }
-
-// Consulta y por medio de un while muestra la lista de los pedidos
-$query = mysqli_query($result,'select p.cliente_id, p.pedido_id as pedido_id, c.nombres as nombres, p.nombre_pedido, p.t_costo, p.start, p.end, p.estado from pedidos p inner join clientes c on p.cliente_id = c.id where p.start > NOW() and estado = 1 ORDER BY p.start ASC;');
-
-$tr = '';
-
- while ($row = $query->fetch_array(MYSQLI_BOTH)){
-
- 	$estado = ($row['estado'] == '0')?"Pendiente":"Realizado";
-
- 	$tr .=	"<tr class='rows' id='rows'>
-				<td>" . $row['nombres'] 		. "</td>
-				<td>" . $row['nombre_pedido'] 	. "</td>
-				<td  align='right'>$ " . $row['t_costo'] 	. "</td>
-				<td>" . $row['start']	. "</td>
-				<td>" . $row['end']	. "</td>
-				<td>" . $estado	. "</td>
-				<td>&nbsp;&nbsp;
-				<a href='pedidoProductos.php?id=" . $row['pedido_id'] . "'><span data-tooltip='Ver Detalles'>
-					<i class='fa fa-file-text-o'></i></spam></a>&nbsp;&nbsp;
-				</td>
-			</tr>";
-
- }
+include "../menu.php";
 
 
 $html="<!DOCTYPE html>
 <head>
-<title>Eventos</title>
+<title>Ingresos</title>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 <meta name='keywords' content='Sistema Administrativo' />
@@ -126,6 +73,9 @@ $html="<!DOCTYPE html>
     $(document).ready(function() {
       $('#table').basictable();
     }); 
+	function abrir(url) { 
+	open(url,'','top=100,left=100,width=900,height=700') ; 
+	}
 </script>
 <script>
 function confirmar(texto)
@@ -145,61 +95,45 @@ else return false;
 			<div class='agile-grids'>	
 				<!-- tables -->
 				
-				<div class='footer col-md-12' $show>
-					<h2>Eventos de hoy</h2>
+				<div class='table-heading'>
+					<h2>Informes</h2>
 				</div>
-				<div class='agile-tables' $show>
-					<div class='w3l-table-info'>
-					    <table id='table'>
-						<thead>
-						  <tr>
-							<th>Cliente</th>
-							<th>Nombre Evento</th>
-							<th>Valor</th>
-							<th>Inicia</th>
-							<th>Finaliza</th>
-							<th>Estado</th>
-							<th>Acciones</th>
-						  </tr>
-						</thead>
-						<tbody>
-						  " 
-						  . $tr2 . 
-						  "
-						</tbody>
-					  </table>
-					</div>
+				<div class='bs-component mb20 col-md-2'>
+					<button type='button' class='btn btn-primary btn-block hvr-icon-float-away' onclick='javascript:abrir(\"../../html/ingreso/nuevoIngreso.php\")'>Anual</button>
+					<button type='button' class='btn btn-primary btn-block hvr-icon-float-away' onclick='javascript:abrir(\"../../html/ingreso/nuevoIngreso.php\")'>Mes anterior</button>
+					<button type='button' class='btn btn-primary btn-block hvr-icon-float-away' onclick='javascript:abrir(\"../../html/ingreso/nuevoIngreso.php\")'>Mes Actual</button>
 				</div>
-
-				<div class='footer'>
-					<h2>Pr&oacute;ximos Eventos</h2>
-				</div>
-
 				<div class='agile-tables'>
 					<div class='w3l-table-info'>
+					  	<h3>Total Ingresos: $ " . number_format($row2['total'], 0, ",", ".") . "</h3>
 					    <table id='table'>
 						<thead>
 						  <tr>
-							<th>Cliente</th>
-							<th>Nombre Evento</th>
+							<th>Fecha</th>
+							<th>Producto</th>
+							<th>Detalles</th>
 							<th>Valor</th>
-							<th>Inicia</th>
-							<th>Finaliza</th>
-							<th>Estado</th>
 							<th>Acciones</th>
 						  </tr>
 						</thead>
 						<tbody>
-						  " 
-						  . $tr . 
-						  "
+						  <tr>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+						  </tr>
 						</tbody>
 					  </table>
 					</div>
 				</div>
+				<!-- //tables -->
+			</div>
+		</div>
 		<!-- footer -->
 		<div class='footer'>
-			<p>© 2018 Wink. All Rights Reserved . Design by <a href='edisonosorioj.com'></a>EdisonOsorioJ</p>
+			<p>© 2017 AdminSoft . All Rights Reserved . Design by <a href='edisonosorioj.com'></a>AlDía</p>
 		</div>
 		<!-- //footer -->
 	</section>
