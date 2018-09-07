d3ded9a83c0f1e74c1451e1526ec5c7f5f71f28e
edisonosorioj <edisonosorioj@gmail.com>
2018-09-06 21:07:28 -0500

    avanzando en creación de ingreso desde pedidos

--- iniciosForPymes/madeira/php/pedidos/hacerPedido.php
+++ iniciosForPymes/madeira/php/pedidos/hacerPedido.php
@@ -75,7 +75,10 @@ if ($estado == 1) {
  $detalles 		= "Pedido No. " . $pedido_id . " - " . $nombre_pedido;
  $valorcredito 	= $cobrado;
 
- $query6 = mysqli_query($result,"INSERT INTO creditos (fecha, detalles, valor, idclientes) VALUES ('$fecha', '$detalles', CONCAT('-','$valorcredito'), '$cliente_id');");
+ // $query6 = mysqli_query($result,"INSERT INTO creditos (fecha, detalles, valor, idclientes) VALUES ('$fecha', '$detalles', CONCAT('-','$valorcredito'), '$cliente_id');");
+
+//Aqui VOY
+ $query6 = mysqli_query($result,"INSERT INTO ingresos (fecha, cantidad, detalles, valor, idclientes) VALUES ('$fecha', '1', '$detalles', '$cliente_id');");
 
 
 // Actualiza la tabla de pedidos con los parametros de total de costo, total cobrado que viene por post y cambia el estado para que este como realizado
